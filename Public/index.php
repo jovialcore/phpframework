5a194ef39e8b72511064d12d7a85b42c22921f8e
Chidiebere Chukwudi <jollydosky@gmail.com>
2023-03-08 18:31:01 +0100

    added middlewares: router, dispatcher and PSR-15 http messaging standards

--- Public/index.php
+++ Public/index.php
@@ -3,7 +3,33 @@
 declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+use DI\ContainerBuilder;
+use DemoPhpframework\genesis;
+use Relay\Relay;
+use Laminas\Diactoros\ServerRequestFactory;
+
+use function DI\create;
+
+//dependency injection but using the libray php-di
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+//$containerBuilder->useAnnotations(false); // annotations are now disabled by default no need to add them
+$containerBuilder->addDefinitions([
+
+    genesis::class => create(genesis::class)
+]);
+
+// understanding namespaces in php https://www.php.net/manual/en/language.namespaces.rationale.php#116280
+
+//what does the container Builder or libray do: it automatically injects a dependecny for your class parameter so you don't have to instantiate the classes (not like the class is not being instantiated but you will not need to use new Keyword, etc)
+$container = $containerBuilder->build();
+
+$middlewareQueue = [];
+
+$requestHandler = new Relay($middlewareQueue);
+$inTheBegining = $container->get(genesis::class);
+
+$inTheBegining->announce();
 
 
-$genesis = new \DemoPhpframework\genesis();
-$genesis->speakTheWord();
+//the laminas diactoros package is to implemnet a PSR-7 compatible HTTP Messages
